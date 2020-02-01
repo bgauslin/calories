c699f23947433a1a75ebad1baa27e7f9627c1d11
Ben Gauslin <github@gauslin.com>
2020-02-01 12:26:57 -0500

    Adds WeeklyLoss interface and object to Constants.ts

--- src/js/modules/Constants.ts
+++ src/js/modules/Constants.ts
@@ -2,6 +2,13 @@ interface Activity {
   description?: string,
   label: string,
   multiplier: number,
+  value: number,
+}
+
+inteface WeeklyLoss {
+  amount: number,
+  label: string,
+  value: number,
 }
 
 const ACTIVITY: Activity[] = [
@@ -9,21 +16,25 @@ const ACTIVITY: Activity[] = [
     description: null,
     label: 'Sedentary',
     multiplier: 1.2,
+    value: 0,
   },
   {
-    description: '1-3 days/week',
+    description: '1-3',
     label: 'Lightly active',
     multiplier: 1.375,
+    value: 1,
   },
   {
-    description: '3-5 days/week',
+    description: '3-5',
     label: 'Moderately active',
     multiplier: 1.55,
+    value: 2,
   },
   {
-    description: '6-7 days a week',
+    description: '6-7',
     label: 'Very active',
     multiplier: 1.725,
+    value: 3,
   },
 ];
 
@@ -32,6 +43,29 @@ WEEKLY_LOSS.set('1/2 lb.', 250);
 WEEKLY_LOSS.set('1 lb.', 500);
 WEEKLY_LOSS.set('1 1/2 lbs.', 750);
 WEEKLY_LOSS.set('2 lbs.', 1000);
+
+const WEEKLY_LOSS_NEW: WeeklyLoss[] = [
+  {
+    amount: 250,
+    label: '1/2 lb.',
+    value: .5,
+  },
+  {
+    amount: 500,
+    label: '1 lb.',
+    value: 1,
+  },
+  {
+    amount: 750,
+    label: '1 1/2 lbs.',
+    value: 1.5,
+  },
+  {
+    amount: 1000,
+    label: '2 lbs.',
+    value: 2,
+  },
+];
 
 enum Attribute {
   NO_TOUCH = 'no-touch',
