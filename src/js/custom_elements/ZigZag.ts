35c9c3087a98aae182734ec8ef85daa63bea9fc6
Ben Gauslin <ben@gauslin.com>
2020-02-22 07:48:50 -0500

    minor ZigZag refactoring

--- src/js/custom_elements/ZigZag.ts
+++ src/js/custom_elements/ZigZag.ts
@@ -1,20 +1,11 @@
-/** Daily zig-zag calorie needs modifiers per day of the week. */
-const ZIGZAG_CALORIES: number[] = [1, .9, 1.1, 1, 1, .8, 1.2];
+const WEEKDAYS: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const WEEKDAYS = [
-  ['Sun', 'Sunday'],
-  ['Mon', 'Monday'],
-  ['Tue', 'Tuesday'],
-  ['Wed', 'Wednesday'],
-  ['Thu', 'Thursday'],
-  ['Fri', 'Friday'],
-  ['Sat', 'Saturday'],
-];
+/** Zig-zag calories modifiers per day of the week. */
+const ZIGZAG_CALORIES: number[] = [1, .9, 1.1, 1, 1, .8, 1.2];
 
 const ZIGZAG_ID: string = 'zig-zag-data';
 
 class ZigZag extends HTMLElement {
-  private chart_: HTMLElement;
   private counters_: NodeList;
 
   constructor() {
@@ -31,36 +22,35 @@ class ZigZag extends HTMLElement {
   }
 
   /**
-   * Renders the DOM for the custom element.
+   * Renders the custom element's DOM and creates reference to its elements
+   * that will udpate on user interaction.
    */
   private setupDom_() {
     let html = `<div id="${ZIGZAG_ID}" class="${ZIGZAG_ID}">`;
     for (let i = 0; i < ZIGZAG_CALORIES.length; i++) {
       html += `\
         <div class="zig-zag__day">\
-          <div class="zig-zag__label">${WEEKDAYS[i][1]}</div>\
+          <div class="zig-zag__label">${WEEKDAYS[i]}</div>\
           <result-counter class="zig-zag__value"></result-counter>\
         </div>\
       `;
     }
     html += '</div>';
-
     this.innerHTML += html.replace(/\s\s/g, '');
 
-    this.chart_ = this.querySelector(`#${ZIGZAG_ID}`);
     this.counters_ = this.querySelectorAll('result-counter');
   }
 
   /**
-   * Updates all results counters with each day's zig-zag value.
+   * Updates all counters with each day's zig-zag value.
    */
   private update_(newValue: string) {
-    ZIGZAG_CALORIES.forEach((day, i) => {
+    for (let i = 0; i < ZIGZAG_CALORIES.length; i++) {
       const dailyValue = (ZIGZAG_CALORIES[i] * parseInt(newValue, 10)).toFixed();
-      const el = <HTMLElement>this.counters_[i]
+      const el = <HTMLElement>this.counters_[i];
       el.setAttribute('value', dailyValue);
       el.setAttribute('increment', '');
-    });
+    }
   }
 }
 
