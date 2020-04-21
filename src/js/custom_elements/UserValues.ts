aae755ab702a39c61a4e5e4aaa30ec87caaf677c
Ben Gauslin <bgauslin@gmail.com>
2020-04-21 07:28:01 -0400

    Refactors UserValues methods into smaller separate methods (no-op)

--- src/js/custom_elements/UserValues.ts
+++ src/js/custom_elements/UserValues.ts
@@ -2,6 +2,21 @@ import {ActivityLevel, Measurements, Sex, WeightGoal} from '../modules/Datasets'
 import {Formulas} from '../modules/Formulas';
 import {Templates} from '../modules/Templates';
 
+interface UserMeasurements {
+  activity: string,
+  age: number,
+  goal: string,
+  height: number,
+  sex: string,
+  weight: number,
+}
+
+interface UserMetrics {
+  bmr: number,
+  tdee: number,
+  tdeeMax: number,
+}
+
 const LOCAL_STORAGE: string = 'values';
 
 enum Attribute {
@@ -81,6 +96,43 @@ class UserValues extends HTMLElement {
    * Creates DOM elements and populates them if there are stored user values.
    */
   private setup_(): void {
+    // Place all fields in an array to simplify looping.
+    const measurementFields = Measurements.map(field => field.name);
+    this.allFields_ = [
+      ...measurementFields,
+      ...Object.values(RadioButtonsGroup),
+    ];
+
+    // Render HTML for input groups and result-counter.
+    this.innerHTML = this.render_();
+
+    // Create references to primary elements.
+    this.formEl_ = this.querySelector('form');
+    this.resultEl_ = this.querySelector(`.${CssClass.RESULT}`);
+
+    // If user data exists, update HTML on page load.
+    if (this.storage_) {
+      this.populateInputs_();
+    }
+
+    // Set attribute on each fancy-marker which will trigger it to set its
+    // marker position and the result element's visibility.
+    [...this.querySelectorAll('fancy-marker')].forEach((marker) => {
+      marker.setAttribute('init', '');
+    });
+    
+    // Add focus/blur listeners to text inputs.
+    this.handleInputFocus_();
+
+    // All done.
+    this.hasSetup_ = true;
+    this.update_(null);
+  }
+
+  /**
+   * Renders HTML for all input groups and result-counter.
+   */
+  private render_(): string {
     const html = `\
       <form class="${CssClass.BASE}__form">\
         ${this.templates_.radioButtonsGroup({
@@ -113,36 +165,7 @@ class UserValues extends HTMLElement {
       </form>\
       <result-counter class="${CssClass.RESULT}"></result-counter>\
     `;
-    this.innerHTML = html.replace(/\s\s/g, '');
-
-    // Create references to primary elements.
-    this.formEl_ = this.querySelector('form');
-    this.resultEl_ = this.querySelector(`.${CssClass.RESULT}`);
-
-    // Place all fields in an array to simplify looping.
-    const measurementFields = Measurements.map(field => field.name);
-    this.allFields_ = [
-      ...measurementFields,
-      ...Object.values(RadioButtonsGroup),
-    ];
-
-    // If user data exists, render it on page load.
-    if (this.storage_) {
-      this.populateInputs_();
-    }
-
-    // Set attribute on each fancy-marker which will trigger it to set its
-    // marker position (and the result's visibility).
-    [...this.querySelectorAll('fancy-marker')].forEach((marker) => {
-      marker.setAttribute('init', '');
-    });
-    
-    // Add focus/blur listeners to text inputs.
-    this.handleInputFocus_();
-
-    // All done.
-    this.hasSetup_ = true;
-    this.update_(null);
+    return html.replace(/\s\s/g, '');
   }
 
   /**
@@ -164,6 +187,140 @@ class UserValues extends HTMLElement {
             el.checked = (el.value === stored[name]);
             break;
         }
+      });
+    });
+  }
+
+  /**
+   * Updates result-counter element after getting all values and passing them
+   * into the BMR, BMI, and TDEE formulas.
+   */
+  private update_(event?: Event): void {
+    if (this.querySelectorAll(':invalid').length === 0) {
+      // Get user measurements and save them for subsequent visits.
+      const measurements = this.getMeasurements_()
+      const {bmr, tdee, tdeeMax} = this.getMetrics_(measurements);
+      localStorage.setItem(LOCAL_STORAGE, JSON.stringify(measurements));
+
+      // Show result-counter and enable input groups.
+      this.showResult_(bmr, tdee, tdeeMax);
+      this.makeGroupsActive_(true);
+
+      // Make result-counter increment its value if a radio button was clicked.
+      if (event) {
+        const target = <HTMLInputElement>event.target;
+        if (target && target.type === 'radio') {
+          this.resultEl_.setAttribute(Attribute.INCREMENT, '');
+        } else {
+          this.resultEl_.removeAttribute(Attribute.INCREMENT);
+        }
+      }
+    } else {
+      // Hide result-counter and disable input groups.
+      this.resultEl_.setAttribute(Attribute.HIDDEN, '');
+      this.makeGroupsActive_(false);
+    }   
+  }
+
+  /**
+   * Returns user's age, height, sex, and weight from form inputs.
+   */
+  private getMeasurements_(): UserMeasurements {
+    const values = {};
+    const formData = new FormData(this.formEl_);
+    this.allFields_.forEach((name) => values[name] = formData.get(name));
+
+    // Convert height and weight to metric regardless of user-selected units.
+    let height: number;
+    let weight: number = values['weight'];
+    const units = this.getAttribute(Attribute.UNITS);
+
+    // TODO(#4): Height and weight fields in metric units.
+    switch (units) {
+      case 'metric':
+        height = Number(values['cm']);
+        weight = Number(values['kg']);
+        break;
+      case 'imperial':
+        height = this.formulas_.cm(Number(values['feet'] * 12) + Number(values['inches']));
+        weight = this.formulas_.kg(weight);
+        break;
+    }
+
+    return {
+      activity: values['activity'],
+      age: Number(values['age']),
+      goal: values['goal'],
+      height,
+      sex: values['sex'],
+      weight,
+    }
+  }
+
+  /**
+   * Returns user's BMR, TDEE, and max TDEE based on their measurements.
+   */
+  private getMetrics_(measurements: UserMeasurements): UserMetrics {
+    // Get factors based on selected values.
+    const activityLevel = ActivityLevel.find((level) => {
+      return measurements['activity'] === level.value;
+    });
+
+    const bmr = this.formulas_.basalMetabolicRate(measurements);
+
+    const goalLevel = WeightGoal.find((level) => {
+      return measurements['goal'] === level.value;
+    });
+
+    // Get Total Daily Energy Expenditure (TDEE).
+    const tdee = this.formulas_.totalDailyEnergyExpenditure({
+      activity: activityLevel.factor,
+      bmr,
+      goal: goalLevel.factor,
+    });
+
+    // Get maximum TDEE for zig-zag chart (maximum activity, no weight loss).
+    const tdeeMax = this.formulas_.totalDailyEnergyExpenditure({
+      activity: ActivityLevel[ActivityLevel.length - 1].factor,
+      bmr,
+      goal: WeightGoal[0].factor,
+    });
+
+    return {bmr, tdee, tdeeMax};
+  }
+
+  /**
+   * Sets attributes on result-counter and zig-zag elements so that they can
+   * update themselves.
+   */
+  showResult_(bmr: number, tdee: number, tdeeMax: number) {
+    this.resultEl_.removeAttribute(Attribute.HIDDEN);
+    this.resultEl_.setAttribute('value', tdee.toFixed(0));
+    this.resultEl_.setAttribute('bmr', bmr.toFixed(0));
+ 
+    const zigZag = document.querySelector('zig-zag')
+    zigZag.setAttribute('tdee', tdee.toFixed());
+    zigZag.setAttribute('max-tdee', tdeeMax.toFixed());
+  }
+
+  /**
+   * Toggles 'inactive' attribute on input groups and sets a 'tabindex' value
+   * on their children's labels to enable/disable keyboard tabbing.
+   */
+  private makeGroupsActive_(show: boolean): void {
+    const tabindex = show ? '0' : '-1';
+
+    INACTIVE_ELEMENTS.forEach((selector) => {
+      const group = this.querySelector(selector);
+
+      if (show) {
+        group.removeAttribute(Attribute.INACTIVE);
+      } else {
+        group.setAttribute(Attribute.INACTIVE, '');
+      }
+
+      [...group.querySelectorAll('label[tabindex]')].forEach((label) => {
+        label.setAttribute('tabindex', tabindex);
       });
     });
   }
@@ -191,110 +348,6 @@ class UserValues extends HTMLElement {
         }
       });
     })
-  }
-
-  /**
-   * Updates 'result' element after getting all values and passing them into
-   * the BMR, BMI, and TDEE formulas.
-   */
-  private update_(event?: Event): void {
-    const values = {};
-    const formData = new FormData(this.formEl_);
-    this.allFields_.forEach((name) => values[name] = formData.get(name));
-
-    // Convert height and weight to metric regardless of user-selected units.
-    let height: number;
-    let weight: number = values['weight'];
-    const units = this.getAttribute(Attribute.UNITS);
-
-    // TODO(#4): Height and weight fields in metric units.
-    switch (units) {
-      case 'metric':
-        height = Number(values['cm']);
-        weight = Number(values['kg']);
-        break;
-      case 'imperial':
-        height = this.formulas_.cm(Number(values['feet'] * 12) + Number(values['inches']));
-        weight = this.formulas_.kg(weight);
-        break;
-    }
-
-    const measurements = {
-      age: Number(values['age']),
-      height,
-      sex: values['sex'],
-      weight,
-    };
-
-    // Get BMR based on measurements.
-    const bmr = this.formulas_.basalMetabolicRate(measurements);
-
-    // Get factors based on selected values for calculating calorie needs.
-    const activityLevel = ActivityLevel.find(level => values['activity'] === level.value);
-    const goalLevel = WeightGoal.find(level => values['goal'] === level.value);
-    const activity = activityLevel.factor;
-    const goal = goalLevel.factor;
-
-    // Get Total Daily Energy Expenditure (TDEE).
-    const tdee = this.formulas_.totalDailyEnergyExpenditure({
-      activity,
-      bmr,
-      goal,
-    });
-
-    // Get maximum TDEE for zig-zag chart (maximum activity, no weight loss).
-    const tdeeMax = this.formulas_.totalDailyEnergyExpenditure({
-      activity: ActivityLevel[ActivityLevel.length - 1].factor,
-      bmr,
-      goal: WeightGoal[0].factor,
-    });
-
-    // Display all fields and save result when all required data is provided.
-    if (this.querySelectorAll(':invalid').length === 0) {
-      if (event) {
-        const target = <HTMLInputElement>event.target;
-        if (target && target.type === 'radio') {
-          this.resultEl_.setAttribute(Attribute.INCREMENT, '');
-        } else {
-          this.resultEl_.removeAttribute(Attribute.INCREMENT);
-        }
-      }
-      this.resultEl_.setAttribute('value', tdee.toFixed(0));
-      this.resultEl_.setAttribute('bmr', bmr.toFixed(0));
-
-      // Show result.
-      this.resultEl_.removeAttribute(Attribute.HIDDEN);
-
-      // Enable fields.
-      INACTIVE_ELEMENTS.forEach((selector) => {
-        const group = this.querySelector(selector);
-        group.removeAttribute(Attribute.INACTIVE);
-        [...group.querySelectorAll('label[tabindex]')].forEach((label) => {
-          label.setAttribute('tabindex', '0');
-        });
-      });
-
-      // Store values.
-      localStorage.setItem(LOCAL_STORAGE, JSON.stringify(values));
-
-      // Set zig-zag attributes so it can update itself.
-      const zigZag = document.querySelector('zig-zag')
-      zigZag.setAttribute('tdee', tdee.toFixed());
-      zigZag.setAttribute('max-tdee', tdeeMax.toFixed());
-
-    } else {
-      // Hide result.
-      this.resultEl_.setAttribute(Attribute.HIDDEN, '');
-
-      // Disable fields.
-      INACTIVE_ELEMENTS.forEach((selector) => {
-        const group = this.querySelector(selector);
-        group.setAttribute(Attribute.INACTIVE, '');
-        [...group.querySelectorAll('label[tabindex]')].forEach((label) => {
-          label.setAttribute('tabindex', '-1');
-        });
-      });
-    }
   }
 }
 
