51bebb503395e8a17f99f86a02f749e8c6bdc457
Ben Gauslin <bgauslin@gmail.com>
2021-02-14 04:53:12 -0500

    simplifies and adds event capturing to radio-buttons

--- src/js/custom_elements/radio_buttons/radio_buttons.ts
+++ src/js/custom_elements/radio_buttons/radio_buttons.ts
@@ -8,17 +8,9 @@ class RadioButtons extends HTMLElement {
 
   constructor() {
     super();
-    this.addEventListener('change', this.update);
+    this.addEventListener('change', this.update, true);
     this.resizeListener = this.update.bind(this);
     window.addEventListener('resize', this.resizeListener);
-  }
-
-  static get observedAttributes(): string[] {
-    return ['pending'];
-  }
-
-  attributeChangedCallback() {
-    this.update();
   }
 
   disconnectedCallback() {
@@ -31,27 +23,22 @@ class RadioButtons extends HTMLElement {
    * via CSS transitions.
    */
   private update() {
-    // Remove 'pending' attribute since it's only needed on initial page load.
-    this.removeAttribute('pending');
-
-    // Get the checked element, then its parent since the checkbox itself has
-    // no dimensions, relies on its sibling to trigger it, and their parent is
-    // the target where CSS layout is applied and what we need for determining
-    // the marker's position.
     const checked = this.querySelector(':checked');
-    const targetEl = checked.parentNode as HTMLElement;
+    if (checked) {
+      const targetEl = checked.parentNode as HTMLElement;
 
-    // The marker and this element rely on relative/absolute positioning, so
-    // subtract this element's position in the viewport from the marker
-    // target's position in order to make the starting edge of this element
-    // equal to zero.
-    const leftPos = targetEl.getBoundingClientRect().left - this.getBoundingClientRect().left;
-    const height = targetEl.clientHeight;
+      // The marker and this element rely on relative/absolute positioning, so
+      // subtract this element's position in the viewport from the marker
+      // target's position in order to make the starting edge of this element
+      // equal to zero.
+      const leftPos = targetEl.getBoundingClientRect().left - this.getBoundingClientRect().left;
+      const height = targetEl.clientHeight;
 
-    // Update custom properties and let the CSS take over.
-    this.style.setProperty('--height', `${height / 16}rem`);
-    this.style.setProperty('--left', `${leftPos / 16}rem`);
-    this.style.setProperty('--width', `${targetEl.clientWidth / 16}rem`);
+      // Update custom properties and let the CSS take over.
+      this.style.setProperty('--height', `${height / 16}rem`);
+      this.style.setProperty('--left', `${leftPos / 16}rem`);
+      this.style.setProperty('--width', `${targetEl.clientWidth / 16}rem`);
+    }
   }
 }
 
