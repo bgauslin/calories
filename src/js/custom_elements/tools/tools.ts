975274b619dac11d566fd5149796ebe8655e7d27
Ben Gauslin <github@gauslin.com>
2022-09-25 06:17:05 -0500

    Removes ga

--- src/js/custom_elements/tools/tools.ts
+++ src/js/custom_elements/tools/tools.ts
@@ -14,7 +14,6 @@ class Tools extends HTMLElement {
     if (!this.hasSetup) {
       this.setupDom();
       this.touchEnabled();
-      this.googleAnalytics();
       this.hasSetup = true;
     }
   }
@@ -24,7 +23,7 @@ class Tools extends HTMLElement {
    */
   private setupDom() {
     document.body.removeAttribute('no-js');
-    document.querySelector('noscript').remove();
+    document.querySelector('noscript')?.remove();
   }
 
   /**
@@ -34,20 +33,6 @@ class Tools extends HTMLElement {
     if ('ontouchstart' in window || (window as any).DocumentTouch) {
       document.body.removeAttribute('no-touch');
       fastclick['attach'](document.body);
-    }
-  }
-
-  /**
-   * Initializes Google Analytics tracking.
-   */
-  private googleAnalytics() {
-    if (process.env.NODE_ENV === 'production') {
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*(new Date() as any);a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      (window as any).ga('create', process.env.GA_ID, 'auto');
-      (window as any).ga('send', 'pageview');
     }
   }
 }
