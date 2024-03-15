8249aa8977246e79acdaf68a298da78d76591c16
Ben Gauslin <github@gauslin.com>
2024-03-15 11:36:09 -0500

    JSON for info-panel fetch instead of GraphQL

--- src/js/custom_elements/info_panel/info_panel.ts
+++ src/js/custom_elements/info_panel/info_panel.ts
@@ -1,3 +1,4 @@
+const ENDPOINT = 'https://gauslin.com/api/etc/calories.json';
 const FOR_ATTR = 'for';
 const PENDING_ATTR = 'pending';
 
@@ -6,7 +7,7 @@ const PENDING_ATTR = 'pending';
  * GraphQL endpoint.
  */
 class InfoPanel extends HTMLElement {
-  private target: Element;
+  private target: HTMLElement;
 
   constructor() {
     super();
@@ -21,43 +22,23 @@ class InfoPanel extends HTMLElement {
    * Adds and removes attributes.
    */
   private setup() {
-    const target = this.getAttribute(FOR_ATTR)
-    this.target = document.getElementById(target);
+    const target = this.getAttribute(FOR_ATTR)!;
+    this.target = document.getElementById(target)!;
     this.removeAttribute(FOR_ATTR);
     this.setAttribute('aria-labelledby', target);
     this.setAttribute('hidden', '');
   }
 
   /**
-   * Fetches data from a GraphQL endpoint.
+   * Fetches JSON data from an API endpoint.
    */
   private async renderPanel(): Promise<any> {
-    const endpoint = (process.env.NODE_ENV === 'production') ?
-        process.env.GRAPHQL_PROD : process.env.GRAPHQL_DEV;
-    const query = `
-      query {
-        entry(collection: "pages", slug: "calories") {
-          ... on Entry_Pages_Pages {
-            content
-          }
-        }
-      }
-    `;
-
     try {
-      const response = await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        },
-        body: JSON.stringify({query}),
-      });
-
-      // Parse data and render it, then let the target know this is ready.
+      const response = await fetch(ENDPOINT);
       const json = await response.json();
+
       const div = document.createElement('div');
-      div.innerHTML = json.data.entry.content;
+      div.innerHTML = json.info;
       this.appendChild(div);
       this.target.removeAttribute(PENDING_ATTR);
     } catch (error) {
