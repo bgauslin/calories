0f173a91e8ee4f1f1a7fb97922834ded241a2298
Ben Gauslin <github@gauslin.com>
2024-03-18 10:56:40 -0500

    removes tools since it's just two lines of JS now

--- src/js/calories.ts
+++ src/js/calories.ts
@@ -3,12 +3,16 @@ require('dotenv').config();
 import './custom_elements/info/info';
 import './custom_elements/number_ticker/number_ticker';
 import './custom_elements/radio_marker/radio_marker';
-import './custom_elements/tools/tools';
 import './custom_elements/user_values/user_values';
 import './custom_elements/zigzag/zigzag';
 
 import '../stylus/index.styl';
 
+// Remove JS guard from HTML.
+document.body.classList.remove('no-js');
+document.querySelector('noscript')!.remove();
+
+// Register the Service Worker.
 if (process.env.NODE_ENV === 'production') {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
