33cb713b7bfa310e0257fe2be7e98c9fa1f043d5
Ben Gauslin <github@gauslin.com>
2024-03-18 07:00:59 -0500

    info toggle and panel merged

--- src/js/calories.ts
+++ src/js/calories.ts
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
-import './custom_elements/info_panel/info_panel';
 import './custom_elements/info_toggle/info_toggle';
 import './custom_elements/number_ticker/number_ticker';
 import './custom_elements/radio_marker/radio_marker';
--- src/js/custom_elements/info_toggle/info_toggle.ts
+++ src/js/custom_elements/info_toggle/info_toggle.ts
@@ -1,26 +1,15 @@
-import smoothscroll from 'smoothscroll-polyfill';
-
-const FOR_ATTR = 'for';
-const HIDDEN_ATTR = 'hidden';
-const OPEN_ATTR = 'open';
-const PENDING_ATTR = 'pending';
-
-const BASE_ATTRIBUTES = [
-  ['role', 'button'],
-  ['aria-haspopup', 'true'],
-  ['aria-controls', 'info-panel'],
-  ['aria-label', 'About this app'],
-  ['aria-expanded', 'false'],
-  [PENDING_ATTR, ''],
-];
+const ENDPOINT = 'https://gauslin.com/api/etc/calories.json';
 
 /**
+ * TODO: Update overview...
  * Custom element that toggles the visibility of a target element.
+ * Custom element that populates itself with data fetched from a JSON endpoint.
  */
 class InfoToggle extends HTMLElement {
   private iconTemplate: any;
   private isOpen: boolean;
-  private target: HTMLElement;
+  private button: HTMLButtonElement;
+  private panel: HTMLElement;
 
   constructor() {
     super();
@@ -28,11 +17,11 @@ class InfoToggle extends HTMLElement {
     this.iconTemplate = require('./info_toggle_icon.pug');
     this.addEventListener('click', this.togglePanel);
     this.addEventListener('keyup', this.handleKey);
-    smoothscroll.polyfill();
   }
 
   connectedCallback() {
-    this.setup();
+    this.renderButton();
+    this.renderPanel();
   }
 
   disconnectedCallback() {
@@ -40,41 +29,68 @@ class InfoToggle extends HTMLElement {
     this.removeEventListener('keyup', this.handleKey);
   }
 
-  private setup() {
-    this.target = document.getElementById(this.getAttribute(FOR_ATTR)!)!;
-    this.removeAttribute(FOR_ATTR);
-    this.tabIndex = 0;
+  private renderButton() {
+    this.button = document.createElement('button');
+    this.button.ariaExpanded = 'false';
+    this.button.ariaLabel = 'About this app';
+    this.button.id = 'info-toggle';
+    this.button.innerHTML = this.iconTemplate({name: 'info'});
+    this.button.tabIndex = 0;
+    this.button.setAttribute('aria-haspopup', 'true');
+    this.button.setAttribute('aria-controls', 'info-panel');
+    this.button.setAttribute('pending', '');
 
-    for (const attribute of BASE_ATTRIBUTES) {
-      const [name, value] = attribute;
-      this.setAttribute(name, value);
-    }
-    this.innerHTML = this.iconTemplate({name: 'info'});
+    this.appendChild(this.button);
   }
 
-  private togglePanel() {
+  private async renderPanel(): Promise<any> {
+    try {
+      const response = await fetch(ENDPOINT);
+      const json = await response.json();
+
+      this.panel = document.createElement('div');
+      this.panel.id = 'info-panel';
+      this.panel.innerHTML = json.info;
+      this.panel.setAttribute('aria-labelledby', 'info-toggle');
+      this.panel.setAttribute('hidden', '');
+
+      this.appendChild(this.panel);
+      this.button.removeAttribute('pending');
+    } catch (error) {
+      console.warn('Currently unable to fetch data. :(');
+      return;
+    }
+  }
+
+  private togglePanel(event: Event) {
+    if (event.target !== this.button) {
+      return;
+    }
+
     if (!this.isOpen) {
       this.openPanel();
     } else {
       this.closePanel();
     }
     this.isOpen = !this.isOpen;
-    this.ariaExpanded = `${this.isOpen}`;
+    this.button.ariaExpanded = `${this.isOpen}`;
   }
 
   private openPanel() {
-    this.innerHTML = this.iconTemplate({name: 'close'});
-    this.target.removeAttribute(HIDDEN_ATTR);
+    this.button.innerHTML = this.iconTemplate({name: 'close'});
+
+    this.panel.removeAttribute('hidden');
     window.requestAnimationFrame(() => {
-      this.target.setAttribute(OPEN_ATTR, '');
+      this.panel.setAttribute('open', '');
     });
   }
 
   private closePanel() {
-    this.innerHTML = this.iconTemplate({name: 'info'});
-    this.target.removeAttribute(OPEN_ATTR);
-    this.target.addEventListener('transitionend', () => {
-      this.target.setAttribute(HIDDEN_ATTR, '');
+    this.button.innerHTML = this.iconTemplate({name: 'info'});
+
+    this.panel.removeAttribute('open');
+    this.panel.addEventListener('transitionend', () => {
+      this.panel.setAttribute('hidden', '');
     }, {once: true});
   }
 
