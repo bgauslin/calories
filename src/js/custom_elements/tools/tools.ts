b998c6f682d333308cd72cc467f4906248eb9c13
Ben Gauslin <github@gauslin.com>
2024-03-18 08:31:48 -0500

    refactors/removes no-touch and no-js body attributes

--- src/js/custom_elements/tools/tools.ts
+++ src/js/custom_elements/tools/tools.ts
@@ -1,5 +1,3 @@
-import fastclick from 'fastclick';
-
 /**
  * Custom element that updates the DOM and initializes site-wide features.
  */
@@ -13,7 +11,6 @@ class Tools extends HTMLElement {
   connectedCallback() {
     if (!this.hasSetup) {
       this.setupDom();
-      this.touchEnabled();
       this.hasSetup = true;
     }
   }
@@ -22,18 +19,8 @@ class Tools extends HTMLElement {
    * Removes 'no JS' stuff from the DOM.
    */
   private setupDom() {
-    document.body.removeAttribute('no-js');
+    document.body.classList.remove('no-js');
     document.querySelector('noscript')!.remove();
-  }
-
-  /**
-   * Removes 'no-touch' attribute and adds fastclick if device is touch-enabled.
-   */
-  private touchEnabled() {
-    if ('ontouchstart' in window || (window as any).DocumentTouch) {
-      document.body.removeAttribute('no-touch');
-      fastclick['attach'](document.body);
-    }
   }
 }
 
