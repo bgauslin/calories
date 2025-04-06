af8d1733289a8bd7e5b1be0e3868d5994d9cf32f
Ben Gauslin <github@gauslin.com>
2025-04-06 09:51:35 +0200

    dialog element wired up

--- src/js/custom_elements/info/info.ts
+++ src/js/custom_elements/info/info.ts
@@ -13,14 +13,14 @@ class Info extends LitElement {
 
   @property({reflect: true, type: Boolean}) hidden: boolean = true;
   @query('button') button: HTMLButtonElement;
-  @query('.dialog') dialog: HTMLElement;
+  @query('dialog') dialog: HTMLDialogElement;
   @state() endpoint: string = 'https://gauslin.com/api/calories.json';
   @state() open: boolean = false;
   @state() json: any;
 
   constructor() {
     super();
-    this.clickListener = this.togglePanel.bind(this);
+    this.clickListener = this.handleClick.bind(this);
     this.keyListener = this.handleKey.bind(this);
   }
 
@@ -33,7 +33,6 @@ class Info extends LitElement {
 
   disconnectedCallback() {
     super.disconnectedCallback();
-    document.removeEventListener('click', this.clickListener);
     document.removeEventListener('keyup', this.keyListener);
   }
 
@@ -52,21 +51,23 @@ class Info extends LitElement {
     }
   }
 
-  private togglePanel(event: Event) {
-    const target = event.target;
-
-    if (target === document.body && this.open) {
+  private togglePanel() {
+    if (this.dialog.open) {
       this.open = false;
+      this.dialog.addEventListener('transitionend', () => {
+        this.dialog.close();
+      }, {once: true});
+    } else {
+      this.dialog.show();
+      this.open = true;
     }
+  }
 
-    if (target !== this.button) {
-      return;
-    }
+  private handleClick(event: Event) {
+    const target = event.composedPath()[0];
 
-    this.open = !this.open;
-
-    if (this.open) {
-      this.dialog.scrollTo(0, 0);
+    if (target === this.button || target === document.body && this.open) {
+      this.togglePanel();
     }
   }
 
@@ -76,36 +77,32 @@ class Info extends LitElement {
     }
   }
 
-  // TODO: a11y for keyboard.
-  // TODO: replace <div.dialog> with <dialog> element.
+  // TODO: Verify ARIA / a11y for <dialog>.
   protected render() {
     if (!this.json) return;
 
     const iconPath = this.open ? 'M5,5 L19,19 M5,19 L19,5'  : 'M9,11 L12,11 L12,18 M9,18 L15,18 M12,12 m11,0 a11,11 0 1,0 -22,0 a11,11 0 1,0 22,0 M11,6 m1,0 a0.5,0.5 0 1,0 -1,0 a0.5,0.5 0 1,0 1,0';
     const iconClass = this.open ? 'close' : 'info';
+    const label = this.open ? 'Close window' : 'About this app';
 
     return html`
       <button
-        aria-controls="info"
-        aria-expanded="${this.open}"
-        aria-haspopup="true"
-        aria-label="About this app"
+        aria-label="${label}"
         id="toggle"
-        tabindex="${this.open ? '0' : '-1'}"
+        title="${label}"
         type="button">
         <svg class="icon icon--${iconClass}" viewbox="0 0 24 24" aria-hidden="true">
           <path d="${iconPath}"/>
         </svg>
       </button>
-      <div
-        aria-labelledby="toggle"  
-        class="dialog"
+      <dialog
         id="info"
-        ?data-open="${this.open}">
+        ?inert="${!this.open}"
+        ?open="${this.open}">
         <article>
           ${unsafeHTML(this.json.info)}
         </article>
-      </div>
+      </dialog>
     `;
   }
 }
